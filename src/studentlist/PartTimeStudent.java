523c4c2f9271dbd21452da9305ea229f9a4b953d
markb <markb@10.0.0.137>
2021-01-27 13:28:15 -0500

    Added Part Time student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,23 @@
+package studentlist;
+
+/**
+ *
+ * @author markb
+ */
+public class PartTimeStudent extends Student{
+    private int numCourses;
+
+    public PartTimeStudent(int numCourses, String name) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+    
+}
