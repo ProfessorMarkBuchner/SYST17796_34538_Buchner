12b49cd6dc01fc110b5d7904e318b9377e1b71a2
markb <markb@10.0.0.137>
2021-01-27 12:22:18 -0500

    First Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,41 @@
+/*
+ * This class is a simple example of creating an array of objects
+ */
+package studentlist;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author markb
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Student[] students = new Student[2];
+    
+
+        for (int i = 0; i < students.length; i++) {
+       
+            System.out.println("Enter the students name");
+            String name = input.nextLine();
+            Student student = new Student(name);
+       
+            students[i] = student;
+        }
+        System.out.println("Printing the students:");
+        String format = "The student's name is %s\n";     
+        for (Student student: students) {
+        System.out.printf(format, student.getName());   
+        
+        }
+ 
+
+       }
+}
+
+    
