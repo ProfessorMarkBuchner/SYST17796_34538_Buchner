c61bc7fce42c3b9a41de9cd0d1f9f4c94fef025e
Roopali <Roopali @192.168.2.20>
2021-02-07 23:34:23 -0500

    Algorithm to search card in magic hand

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -6,6 +6,8 @@
 
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
+import java.util.Scanner;
+
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -27,11 +29,26 @@ public class CardTrick {
                 count++;
             }
         }
-                
-        for(int i=0; i<magicHand.length; i++){
-            System.out.println(magicHand[i]);
-        }
 
+        System.out.println("Pick any card");
+        System.out.println("Enter 1 for Hearts");
+        System.out.println("Enter 2 for Diamonds");
+        System.out.println("Enter 3 for Spades");
+        System.out.println("Enter 4 for Clubs");
+        
+        Scanner scanner = new Scanner(System.in);
+        String suitIndex = scanner.nextLine();
+        String suit = Card.SUITS[Integer.parseInt(suitIndex)-1];
+        
+        System.out.println("Enter a number between 1 to 13");
+        String valueString = scanner.nextLine();
+        int value = Integer.parseInt(valueString);
+        
+        Card userCard = new Card(suit, value);
+        if(checkCardExistsInMagicHand(userCard, magicHand))
+            System.out.println("The card exists in magic hand");
+        else
+            System.out.println("The card doesn't exist in magic hand");
     }
 
     private static Card getRandomCard() {
