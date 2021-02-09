d7b67d1c40c53a938780f257d5ba142a8a2aeec5
Nicole <Nicole@192.168.0.107>
2021-02-09 03:10:58 -0500

    Final code.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 991599290
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
@@ -11,13 +9,13 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
+ * @modifier Nicole Sheppard
  * @author dancye
  */
 public class Card {
 
    private String suit; //clubs, spades, diamonds, hearts
-   private int value;//1-13
+   private int value; //1-13
 
    public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
     /**
--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,14 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 991599290
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ * @modifier Nicole Sheppard
  * @author dancye
  */
 public class CardTrick {
@@ -17,16 +17,60 @@ public class CardTrick {
     {
         Card[] magicHand = new Card[7];
         
+        // Creating the magic hand
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*13)+1);
+            c.setSuit(Card.SUITS[(int)(Math.random()*4)]);
+            
+            magicHand[i] = c;
         }
         
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
+        Scanner keysIn = new Scanner(System.in);
+        
+        System.out.println("Pick a card, any card! Choose a value and suit!");
+        
+        // Prompting user for value of their card
+        System.out.print("Pick a value (1 - 13): ");
+        int userValue = keysIn.nextInt();
+        while (userValue < 1 || userValue > 13)
+        {
+            System.out.print("Please only enter a value between 1 and 13: ");
+            userValue = keysIn.nextInt();
+        } 
+        
+        // Prompting user for suit of their card
+        System.out.print("Pick a suit (Hearts, Diamonds, Spades, Clubs): ");
+        String userSuit = keysIn.next();
+        while (!userSuit.equalsIgnoreCase("Hearts") && !userSuit.equalsIgnoreCase("Spades") && !userSuit.equalsIgnoreCase("Clubs") && !userSuit.equalsIgnoreCase("Diamonds"))
+        {
+            System.out.print("Please only enter Hearts, Spades, Clubs, or Diamonds: ");
+            userSuit = keysIn.next();
+        }
+        
+        System.out.println("You picked "+userValue+" of "+userSuit);
+        
+        // Checking array to see if user input matches magic hand
+        boolean winner = false;
+        for (int i = 0; i < magicHand.length; i++)
+        {
+            if (userValue == magicHand[i].getValue())
+            {
+                if (userSuit.equalsIgnoreCase(magicHand[i].getSuit()))
+                {
+                    System.out.println("Congratulations! Your card was in the magic hand!");
+                    winner = true;
+                }
+                                   
+            }           
+        }
+        
+        // Result for loss
+        if (winner == false)
+        {
+            System.out.println("Your card was not in the magic hand.");
+        }
     }
     
 }
